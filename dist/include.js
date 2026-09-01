9142785e06ece26ab4eb260ace2238b7b24b7b89
Eric Rotramel <eric@welcometopull.com>
2026-09-01 17:56:16 -0400

    Add files via upload

--- dist/include.js
+++ dist/include.js
@@ -1,17 +1,16 @@
 async function includeHTML() {
-      const response = await fetch('https://ieahts.org/whatsnew.html', { cache: 'no-store' });
-      if (response.ok) {
-          const content = await response.text();
-          const targetElement = document.getElementById('whatsnew');
-          if (targetElement) { // Add a check to prevent error if element somehow still null
-              targetElement.innerHTML = content;
-          } else {
-              console.error('Target element with ID "whatsnew" not found.');
-          }
-      } else {
-          console.error('Failed to load HTML file:', response.status, response.statusText);
-      }
-  }
-  
-  // Call includeHTML after the DOM is fully loaded
-  document.addEventListener('DOMContentLoaded', includeHTML);
+    const response = await fetch(`https://ieahts.org/whatsnew.html?v=${Date.now()}`, { cache: 'no-store' });
+    if (response.ok) {
+        const content = await response.text();
+        const targetElement = document.getElementById('whatsnew');
+        if (targetElement) {
+            targetElement.innerHTML = content;
+        } else {
+            console.error('Target element with ID "whatsnew" not found.');
+        }
+    } else {
+        console.error('Failed to load HTML file:', response.status, response.statusText);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', includeHTML);
